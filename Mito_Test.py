3f9f257486e60743aa526fada47635a2d2b48e0f
= <aymancoom3@gmail.com>
2023-11-04 11:06:03 +0200

    Refactor Code

--- Mito_Test.py
+++ Mito_Test.py
@@ -5,12 +5,21 @@ import pandas as pd
 import jwt
 
 queryStringObject = st.experimental_get_query_params()
-tokenQuery = queryStringObject['name'][0]
-print(tokenQuery)
-# !  http://localhost:8501/?name=ayman
-# & {'name': ['ayman']}
+if (queryStringObject):
+    tokenQuery = queryStringObject['name'][0]
 
-# https://pivotanalytica.streamlit.app/
+
+def doThePivotCode(new_dfs, code):
+    st.write(new_dfs)
+    st.code(code)
+    dataframes = list(new_dfs.keys())[1:]
+    print()
+    if st.button('Save Pivots'):  # ! This Name For Button Is Made by programmer
+        with open("my_script.py", "w") as file:
+            file.write(code)
+            # file.write("dataframes = {}".format(list(new_dfs.keys())[1:]))
+
+
 def renderDataOnTable(dbName, dbSqlQuery):
     server = '10.10.10.100'
     database = dbName
@@ -22,37 +31,21 @@ def renderDataOnTable(dbName, dbSqlQuery):
 
     st.set_page_config(layout="wide")
     st.title('MITO SHEET')
-
     dataFrame = pd.read_sql(query_2, connection)
-
     new_dfs, code = spreadsheet(dataFrame, df_names=['dataFrame'])
+    doThePivotCode(new_dfs, code)
 
 
 try:
-    resPonse = jwt.decode(tokenQuery, "simpleKey",
-                          audience="urn:foo", algorithms=["HS256"])
-    print("Res = ")
-    print(resPonse)
+    key = "simpleKey"
+    aud = "urn:foo"
+    alg = ["HS256"]
+    resPonse = jwt.decode(tokenQuery, key, audience=aud, algorithms=alg)
     dbName = resPonse['dbName']
     dbSqlQuery = resPonse['sqlQuery']
     renderDataOnTable(dbName, dbSqlQuery)
 except Exception:
     # raise ExpiredSignatureError("Signature has expired")
+    st.set_page_config(layout="wide")
+    st.title('Access Denied')
     print("An exception occurred")
-
-
-# Get the Query String From the URL
-# It is a JWT so you Need to parse it
-# It has a Timing and Expires , If it is Valid , Make the API reuqset and Build it
-# else Show a 404 error page and Close Everything
-
-
-# st.write(new_dfs)
-# st.code(code)
-
-# dataframes = list(new_dfs.keys())[1:]
-# print()
-# if st.button('Save Pivots'):
-#     with open("my_script.py", "w") as file:
-#         file.write(code)
-#         file.write("dataframes = {}".format(list(new_dfs.keys())[1:]))
