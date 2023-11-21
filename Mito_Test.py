6630f787afd7d56238dc4234ab97d0a426fb6f4b
Ayman <ayman.salah@2coom.com>
2023-11-21 13:01:04 +0200

    Multiple Pivots

--- Mito_Test.py
+++ Mito_Test.py
@@ -6,40 +6,56 @@ import jwt
 from mitosheet.public.v3 import *
 import requests
 import json
-from RestrictedPython import compile_restricted
-from RestrictedPython import safe_globals
+import re
 
-
-api_url = "http://10.10.10.66:8002/api/save-pivot"
-
+# api_url = "http://10.10.10.66:8002/api/save-pivot"
+api_url = "http://127.0.0.1:8000/api/save-pivot"
 queryStringObject = st.experimental_get_query_params()
 if (queryStringObject):
     tokenQuery = queryStringObject['name'][0]
 
 
-def writePivotIntoFile(pivotCode):
+def handleOneOrBulkPivots(codeString):
+    pivot_lines = re.findall(
+        r'# Pivoted dataFrame into (dataFrame_pivot|\S+_\d*)', codeString)
+    df_names = []
+    for i, name in enumerate(pivot_lines):
+        if i == 0:
+            df_names.append('dataFrame_pivot')
+        else:
+            df_names.append(f'dataFrame_pivot_{i}')
+    df_names.insert(0, 'dataFrame')
+    output_string = "new_dfs, code = spreadsheet({0}, df_names={1})\n".format(
+        ','.join(df_names), str(df_names))
+    return output_string
+
+# ! 4 No Need For it now
+
+
+def writePivotIntoFile(pivotCodeList):
     with open("my_script.py", "w") as file:
-        file.write(pivotCode)
+        # This is a List so you Will need to Parse Later
+        file.write(pivotCodeList)
 
 
-def doThePivotCode(new_dfs, code, queryId):
-    finalFile = ''
+def doThePivotCode(new_dfs, code, queryId, userId):
     st.write(new_dfs)
     st.code(code)
     dataframes = list(new_dfs.keys())[1:]
     fileContent = ''
-    print()
     if st.button('Save Pivots'):
         with open("my_script.py", "w") as file:
             file.write('from mitosheet.streamlit.v1 import spreadsheet\n')
             file.write(code)
-            file.write(
-                'new_dfs, code = spreadsheet(dataFrame, dataFrame_pivot, df_names=[\'dataFrame\', \'dataFrame_pivot\'])\n')
+            lastLineOfCode = handleOneOrBulkPivots(code)
+            # file.write('new_dfs, code = spreadsheet(dataFrame, dataFrame_pivot, df_names=[\'dataFrame\', \'dataFrame_pivot\'])\n')
+            file.write(lastLineOfCode)
         with open('my_script.py', 'r') as file:
             fileContent = file.read()
             data = {
                 "fileContent": fileContent,
                 "queryId": queryId,
+                "userId": userId
             }
             json_data = json.dumps(data)
             headers = {"Content-Type": "application/json"}
@@ -50,26 +66,27 @@ def doThePivotCode(new_dfs, code, queryId):
                 print("Request failed with status code:", response.status_code)
 
 
-def renderDataOnTable(dbName, dbSqlQuery, isAdmin, pivotCode, queryId):
+def renderDataOnTable(dbName, dbSqlQuery, isAdmin, pivotCodeList, queryId, userId):
     server = '10.10.10.100'
     database = dbName
     username = 'ayman'
     password = 'admin@1234'
-    connection_string = f"DRIVER={{/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+    connection_string = f"DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+    # connection_string = f"DRIVER={{/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.4.1}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
     connection = pyodbc.connect(connection_string)
     query_2 = (dbSqlQuery)
     st.set_page_config(layout="wide")
     st.title('MITO SHEET')
     dataFrame = pd.read_sql(query_2, connection)
-
-    if pivotCode != None:
-        writePivotIntoFile(pivotCode)
+    if pivotCodeList:
+        # ! This is Userless Code , Just Writing it to know it
+        writePivotIntoFile(pivotCodeList)
+        # ! Here will be a loop Over the Codes To make ALL pivots One By One
         exec_globals = {'dataFrame': dataFrame}
-        exec(pivotCode, exec_globals)
+        exec(pivotCodeList, exec_globals)
     else:
         new_dfs, code = spreadsheet(dataFrame, df_names=['dataFrame'])
-        if (isAdmin):  # ! If Admin Give him Permission To see the "Save Button" For the Code
-            doThePivotCode(new_dfs, code, queryId)
+        doThePivotCode(new_dfs, code, queryId, userId)
 
 
 try:
@@ -78,12 +95,17 @@ try:
     alg = ["HS256"]
     resPonse = jwt.decode(tokenQuery, key, algorithms=alg,
                           audience=aud, options={"verify_exp": False},)
+    queryId = resPonse['queryId']
     dbName = resPonse['dbName']
     dbSqlQuery = resPonse['sqlQuery']
+    pivotCodeList = resPonse['pivotCode']
     isAdmin = resPonse['isAdmin']
-    pivotCode = resPonse['pivotCode']
-    queryId = resPonse['queryId']
-    renderDataOnTable(dbName, dbSqlQuery, isAdmin, pivotCode, queryId)
+    userId = resPonse['userId']
+
+    print('///////////////////////////////////')
+    print(pivotCodeList)
+    renderDataOnTable(dbName, dbSqlQuery, isAdmin,
+                      pivotCodeList, queryId, userId)
 except Exception:
     # ExpiredSignatureError
     st.set_page_config(layout="wide")
--- my_script.py
+++ my_script.py
@@ -1,21 +1,24 @@
 from mitosheet.streamlit.v1 import spreadsheet
 from mitosheet.public.v3 import *
-
-
 def renderAlsoPivot(dataFrame):
-    # Pivoted dataFrame into dataFrame_pivot
-    tmp_df = dataFrame[dataFrame['ItemCode'].notnull()]
-    tmp_df = tmp_df[['LWhs', 'STOCK']].copy()
-    pivot_table = tmp_df.pivot_table(
-        index=['STOCK'],
-        values=['LWhs'],
-        aggfunc={'LWhs': ['count']}
-    )
-    pivot_table = pivot_table.set_axis(
-        [flatten_column_header(col) for col in pivot_table.keys()], axis=1)
-    dataFrame_pivot = pivot_table.reset_index()
-    new_dfs, code = spreadsheet(dataFrame, dataFrame_pivot, df_names=[
-                                'dataFrame', 'dataFrame_pivot'])
-
-
+	# Pivoted dataFrame into dataFrame_pivot
+	tmp_df = dataFrame[['WhsCode']].copy()
+	pivot_table = tmp_df.pivot_table(
+	    index=['WhsCode'],
+	    values=['WhsCode'],
+	    aggfunc={'WhsCode': ['count']}
+	)
+	pivot_table = pivot_table.set_axis([flatten_column_header(col) for col in pivot_table.keys()], axis=1)
+	dataFrame_pivot = pivot_table.reset_index()
+	
+	# Pivoted dataFrame into dataFrame_pivot_1
+	tmp_df = dataFrame[['ItemCode', 'RWhs']].copy()
+	pivot_table = tmp_df.pivot_table(
+	    index=['ItemCode'],
+	    values=['RWhs'],
+	    aggfunc={'RWhs': ['count']}
+	)
+	pivot_table = pivot_table.set_axis([flatten_column_header(col) for col in pivot_table.keys()], axis=1)
+	dataFrame_pivot_1 = pivot_table.reset_index()
+	new_dfs, code = spreadsheet(dataFrame,dataFrame_pivot,dataFrame_pivot_1, df_names=['dataFrame', 'dataFrame_pivot', 'dataFrame_pivot_1'])
 renderAlsoPivot(dataFrame)
