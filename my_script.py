b48aca6265455b698c28ae381134b24928791bd4
Ayman <ayman.salah@2coom.com>
2023-12-10 10:04:38 +0200

    Edit

---
+++ my_script.py
@@ -0,0 +1,13 @@
+from mitosheet.streamlit.v1 import spreadsheet
+from mitosheet.public.v3 import *
+
+# Pivoted dataFrame into dataFrame_pivot
+tmp_df = dataFrame[['STOCK', 'RWhs']].copy()
+pivot_table = tmp_df.pivot_table(
+    index=['STOCK'],
+    values=['RWhs'],
+    aggfunc={'RWhs': ['count']}
+)
+pivot_table = pivot_table.set_axis([flatten_column_header(col) for col in pivot_table.keys()], axis=1)
+dataFrame_pivot = pivot_table.reset_index()
+new_dfs, code = spreadsheet(dataFrame,dataFrame_pivot, df_names=['dataFrame', 'dataFrame_pivot'])
